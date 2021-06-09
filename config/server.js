ef6e7526b837b194db3932040c81507c3ad79e3a
Shankalpa <shankalpa.lamichhane@gmail.com>
2021-06-09 15:03:34 +0545

    set for production

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e10cb848622b5ec305ca9b26f2a06f52'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4cc93a08-91bf-478f-8675-1dc9c764e4d1'
+};
